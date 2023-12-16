5f38d182925ea25ad04ee092dd5e1f6aff42cb94
pamelagiselle8 <pamelaramirez@unitec.edu>
2023-12-16 09:36:27 -0600

    Avatar en perfil

--- components/DemoAvatar.jsx
+++ components/DemoAvatar.jsx
@@ -2,7 +2,7 @@ import { Avatar } from "tamagui";
 
 export function DemoAvatar({ imageUrl }) {
   return (
-    <Avatar circular size="$6">
+    <Avatar circular size="$10">
       <Avatar.Image accessibilityLabel="Nate Wienert" src={imageUrl} />
       <Avatar.Fallback delayMs={600} backgroundColor="$blue10" />
     </Avatar>
--- screens/Perfil.jsx
+++ screens/Perfil.jsx
@@ -1,11 +1,11 @@
 import { Heading, H2, H5, ScrollView, View, Image, Separator } from "tamagui";
-import { UserCog, ChevronRight, Moon, Star, Sun } from '@tamagui/lucide-icons'
+import { UserCog, ChevronRight, Moon, Star, Sun } from "@tamagui/lucide-icons";
 import { DemoCard } from "../components/DemoCard";
 import { Dimensions, Text } from "react-native";
-import { Check } from '@tamagui/lucide-icons'
-import { Checkbox } from 'tamagui'
+import { Check } from "@tamagui/lucide-icons";
+import { Checkbox } from "tamagui";
 import { useEffect } from "react";
-import { ListItem, XStack, YGroup } from 'tamagui'
+import { ListItem, XStack, YGroup } from "tamagui";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { CerrarUsuario } from "../API/Usuarios_Api";
 
@@ -14,6 +14,7 @@ import {
   Rubik_300Light,
   Rubik_700Bold,
 } from "@expo-google-fonts/rubik";
+import DemoAvatar from "../components/DemoAvatar";
 // import { getHospedajes } from "../API/APICasas";
 
 const windowWidth = Dimensions.get("window").width;
@@ -36,21 +37,35 @@ export function Inicio({ navigation }) {
 
   return (
     <SafeAreaView>
-    <View></View>
-    <View paddingTop="100%">
-        <YGroup alignSelf="center" bordered width={"90%"} size="$5" separator={<Separator />}>
-        <YGroup.Item >
-          <ListItem
-            hoverTheme
-            pressTheme
-            title="Cerrar Sesion"
-            subTitle="Subtitle"
-            icon={undefined}
-            iconAfter={ChevronRight}
-            backgroundColor={"white"}
-            onPress={test}
-          />
-        </YGroup.Item>
+      <View alignItems="center" paddingTop="40%">
+        <DemoAvatar
+          imageUrl={
+            Math.random(1) + 0 === 1
+              ? "https://img.freepik.com/vector-premium/dibujos-animados-perfil-mujer_18591-58480.jpg?w=2000"
+              : "https://img.freepik.com/vector-premium/perfil-hombre-dibujos-animados_18591-58482.jpg?size=626&ext=jpg"
+          }
+        />
+      </View>
+      <View paddingTop="10%">
+        <YGroup
+          alignSelf="center"
+          bordered
+          width={"90%"}
+          size="$5"
+          separator={<Separator />}
+        >
+          <YGroup.Item>
+            <ListItem
+              hoverTheme
+              pressTheme
+              title="Cerrar Sesión"
+              subTitle="Iniciar sesión desde otra cuenta"
+              icon={undefined}
+              iconAfter={ChevronRight}
+              backgroundColor={"white"}
+              onPress={test}
+            />
+          </YGroup.Item>
         </YGroup>
       </View>
     </SafeAreaView>
